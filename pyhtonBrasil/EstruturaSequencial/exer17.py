d76226bce63befe9cee78422949285d8568c3365
Emily B Comin <emilybc.gc@gmail.com>
2024-02-16 18:02:01 -0300

     :seedling: novos exercicios

--- pyhtonBrasil/EstruturaSequencial/exer17.py
+++ pyhtonBrasil/EstruturaSequencial/exer17.py
@@ -9,7 +9,26 @@ comprar apenas galões de 3,6 litros;
 misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias
 '''
 
-...
+tamanho = float(input("Digite o tamanho em metros quadrados: "))
+
+litros = tamanho / 6
+latas = int(litros / 18)
+galoes = float(litros /3.6)
+
+if latas % 18 != 0:
+    latas += 1
+
+print(f'Total de litros na obra: {litros:.2f}')
+print(f'Total de latas necessarias : {latas}')
+
+if galoes % 3.6 != 0:
+    galoes += 1
+
+print(f'Total de Galões necessarios: {galoes:.2f}')
+
+tamanho_margem = tamanho +( tamanho * 0.1)
+    
+
 
 
 
