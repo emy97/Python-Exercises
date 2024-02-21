76bef8c60c88156111689d8479734e9f35b65189
Emily B Comin <emilybc.gc@gmail.com>
2024-02-21 15:54:05 -0300

    novos exercicios

---
+++ pyhtonBrasil/EstruturaDeRepeticao/exer3.py
@@ -0,0 +1,44 @@
+'''
+
+Faça um programa que leia e valide as seguintes informações:
+Nome: maior que 3 caracteres;
+Idade: entre 0 e 150;
+Salário: maior que zero;
+Sexo: 'f' ou 'm';
+Estado Civil: 's', 'c', 'v', 'd';
+
+'''
+
+while True:
+    try:
+        nome = input('Digite seu nome: ')
+        idade = int(input('Digite sua idade: '))
+        salario = float(input('Digite seu salario: R$ '))
+        sexo = input('Digite seu Sexo  [f-feminino] ou [m - masculino]: ').lower()
+        estado_civil = input('Digite seu estado civil [s - solteiro], [c - casado], [v - viuvo] ou [d - divorciado]: ').lower()
+
+    except:
+        print('Digite valores validos !!')
+        continue
+    if len(nome) < 3:
+        print('Digite um nome maior que 3 caracteres !')
+        continue
+
+    elif idade < 0 and idade > 150:
+        print('Digite uma idade entre 0 a 150 !')
+        continue
+
+    elif salario < 0.0:
+        print('Digite um salario maior que 0 ')
+        continue
+
+    elif sexo != 'f' and sexo != 'm':
+        print('Digite sexo valido, f ou m !! ')
+        continue
+    elif estado_civil != 's' and estado_civil != 'c' and estado_civil != 'v' and estado_civil != 'd' :
+        print('Digite sexo valido,[s - solteiro], [c - casado], [v - viuvo] ou [d - divorciado]!! ')
+        continue
+
+    else:
+        print('Tudo certo')
+        break
